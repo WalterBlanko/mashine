bc97f29b1f3ba681e6449a76e41c2c1e337dd34a
WalterBlanko <victor_aponte_bravo@hotmail.com>
2023-07-13 20:34:07 -0400

    jfksdl

--- venv/app.py
+++ venv/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify, request
+from flask import Flask, render_template, request
 from flask_cors import CORS
 import joblib
 import pandas as pd
+import requests
 
 app = Flask(__name__)
 CORS(app)
@@ -9,29 +10,64 @@ CORS(app)
 # Cargar el modelo entrenado
 clf = joblib.load('modelo_entrenado2.pkl')
 
+def make_post_request(url, data):
+    try:
+        response = requests.post(url, json=data)
+        response.raise_for_status()  # Verificar si hay errores en la respuesta HTTP
+
+        # Devolver la respuesta JSON
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print("Error en la solicitud POST:", e)
+
+url = 'https://fastfoodapi.herokuapp.com/predict'
+
 @app.route("/")
 def home():
-    return 'Funciona?'
+    return render_template('index.html')
 
 @app.route("/predecir", methods=["POST"])
 def predecir():
-    json = request.get_json(force=True)
-    medidas = json['Medidas']
+    # Obtener los datos ingresados por el usuario
+    match_kills = int(request.form['MatchKills'])
+    match_assists = int(request.form['MatchAssists'])
+    match_headshots = int(request.form['MatchHeadshots'])
+    map_selection = int(request.form['Map'])
+    internal_team_id = int(request.form['InternalTeamId'])
+    email = request.form['Email']
 
-    # Crear un DataFrame con las medidas y el mismo orden de características
+    # Crear un DataFrame con las medidas en el mismo orden de características
     datos = pd.DataFrame({
-        'MatchKills': [medidas[0]],
-        'MatchAssists': [medidas[1]],
-        'MatchHeadshots': [medidas[2]],
-        'Map': [medidas[3]],
-        'InternalTeamId': [medidas[4]]
+        'MatchKills': [match_kills],
+        'MatchAssists': [match_assists],
+        'MatchHeadshots': [match_headshots],
+        'Map': [map_selection],
+        'InternalTeamId': [internal_team_id]
     })
 
     # Realizar la predicción utilizando el modelo cargado
     prediccion = clf.predict(datos)[0]  # Obtener el valor de la predicción
     porcentaje = round(prediccion * 100)  # Redondear el porcentaje
 
-    return 'Los datos que proporcionaste corresponden a un {}% de probabilidades de ganar.'.format(porcentaje)
+    # Crear el objeto de datos a enviar en el cuerpo del POST
+    data = {
+        'email': email,
+        'MatchKills': match_kills,
+        'MatchAssists': match_assists,
+        'MatchHeadshots': match_headshots,
+        'Map': map_selection,
+        'InternalTeamId': internal_team_id,
+        'prediction': porcentaje
+    }
+
+    make_post_request(url, data)
+
+    return render_template('result.html', result=porcentaje)
+
+@app.route("/result")
+def result():
+    return render_template('result.html')
+
 
 if __name__ == '__main__':
     app.run()
